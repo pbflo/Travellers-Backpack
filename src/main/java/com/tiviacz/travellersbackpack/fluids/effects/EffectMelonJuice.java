d3d58deb085ee17d98ad272386c113ea575ed9d1
pbflo <56795244+pbflo@users.noreply.github.com>
2019-10-31 10:26:08 +0100

    Create EffectMelonJuice.java

---
+++ src/main/java/com/tiviacz/travellersbackpack/fluids/effects/EffectMelonJuice.java
@@ -0,0 +1,32 @@
+package com.tiviacz.travellersbackpack.fluids.effects;
+
+import com.tiviacz.travellersbackpack.api.FluidEffect;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+
+public class EffectMelonJuice extends FluidEffect
+{
+    public EffectMelonJuice()
+    {
+        super(FluidRegistry.getFluid("melonjuice"), 30);
+    }
+    
+    /*
+    @Override
+    public void affectDrinker(FluidStack fluidStack, World world, Entity entity)
+    {
+        if(entity instanceof EntityPlayer)
+        {
+            ((EntityPlayer)entity).clearActivePotions();
+            EntityPlayer player = (EntityPlayer) entity;
+            player.addPotionEffect(new PotionEffect(MobEffects.HASTE, timeInTicks, 0));
+            FluidEffectRegistry.WATER_EFFECT.affectDrinker(world, player);
+        }
+    }
+    */
+}
